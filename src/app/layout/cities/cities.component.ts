d227ec482e6ea65df22d864bcf90915dfd10535b
Leonardo Javier Alviarez Hernández <alviarez.leonardo@gmail.com>
2018-11-06 14:31:28 -0400

    Added table of cities

--- src/app/layout/cities/cities.component.ts
+++ src/app/layout/cities/cities.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
 
 @Component({
   selector: 'app-cities',
   templateUrl: './cities.component.html',
-  styleUrls: ['./cities.component.scss']
+  styleUrls: ['./cities.component.scss'],
+  animations: [routerTransition()]
 })
 export class CitiesComponent implements OnInit {
 
--- src/app/layout/cities/cities.module.ts
+++ src/app/layout/cities/cities.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { CitiesRoutingModule } from './cities-routing.module';
 import { CitiesComponent } from './cities.component';
+import { PageHeaderModule } from './../../shared';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    CitiesRoutingModule
+    CitiesRoutingModule,
+    PageHeaderModule
   ],
   declarations: [
     CitiesComponent
